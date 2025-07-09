cafcfa2a05f7f7b355907fb9bc91ce18a1d9d236
Yi-Fu Wu <yifu.wu@gmail.com>
2025-07-09 14:12:35 -0700

    Use model_type for converter
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- nemo_rl/models/megatron/converters/common.py
+++ nemo_rl/models/megatron/converters/common.py
@@ -274,23 +274,19 @@ class MegatronToHFConverter:
         global_keys = ep_gathered_global_keys
         global_keys_map = {k: None for k in global_keys}
 
-        if "qwen" in hf_model_name.lower():
+        if config.model_type == "qwen":
             self.export_mapping = qwen2_converter.get_export_mapping(megatron_model)
             self.export_transforms = qwen2_converter.get_export_transforms(config)
             self.get_source_fn = lambda source_state_dict, _: _ModelState(
                 source_state_dict
             )
-        elif "llama" in hf_model_name.lower():
+        elif config.model_type == "llama":
             self.export_mapping = llama_converter.get_export_mapping()
             self.export_transforms = llama_converter.get_export_transforms(config)
             self.get_source_fn = lambda source_state_dict, _: _ModelState(
                 source_state_dict
             )
-        elif (
-            "deepseek" in hf_model_name.lower()
-            or "Moonlight-16B-A3B" in hf_model_name
-            or hf_model_name in ("ByteDance-Seed/academic-ds-9B",)
-        ):
+        elif config.model_type in ("deepseek_v2", "deepseek_v3"):
             self.export_mapping = deepseek_converter.get_export_mapping(
                 source=global_keys_map,
                 source_config=megatron_model.config.__dict__,
@@ -299,7 +295,7 @@ class MegatronToHFConverter:
             self.get_source_fn = deepseek_converter.get_source_fn
         else:
             raise ValueError(
-                f"No converter mapping and transforms found for {hf_model_name}"
+                f"No converter mapping and transforms found for {hf_model_name} with model_type {config.model_type}"
             )
 
         self.export_transforms = update_transforms_for_nemorl(self.export_transforms)
