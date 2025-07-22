890be95fcacb78003e3e9ed6f5799da82503876b
ashors1 <ashors@nvidia.com>
2025-07-22 15:32:51 -0700

    update metrics, cleanup
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- tests/test_suites/llm/sft-llama3.2-1b-1n8g-fsdp2tp1.v3.sh
+++ tests/test_suites/llm/sft-llama3.2-1b-1n8g-fsdp2tp1.v3.sh
@@ -33,7 +33,6 @@ uv run tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
 
 # Only run metrics if the target step is reached
 if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
-    # TODO: UPDATE FOR OPENMATHINSTRUCT2
     uv run tests/check_metrics.py $JSON_METRICS \
         'data["train/loss"]["1"] < 0.5' \
         'data["train/loss"]["250"] < 0.5' \
