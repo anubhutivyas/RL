94ff0b73121f1fef0f90f3c8af46bfd58fceb3e9
Anna Shors <ashors@nvidia.com>
2025-07-30 09:21:31 -0700

    fix metric checks
    Signed-off-by: Anna Shors <ashors@nvidia.com>

--- tests/test_suites/llm/sft-llama3.1-70b-8n8g-tp4pp2-long-megatron.sh
+++ tests/test_suites/llm/sft-llama3.1-70b-8n8g-tp4pp2-long-megatron.sh
@@ -36,5 +36,5 @@ if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | ma
     uv run tests/check_metrics.py $JSON_METRICS \
         'data["train/loss"]["1"] < 0.55' \
         'data["train/loss"]["300"] < 0.285' \
-        'max(data["ray/node.0.gpu.0.mem_gb"]) < 50'
+        'max(data["ray/node.0.gpu.0.mem_gb"]) < 70'
 fi
